2414c57f2264cc850f581f920dd167cb53430844
Łukasz Pękala <lukasz-pekala@users.noreply.github.com>
2021-10-20 19:17:21 +0200

    LoginValidatorTests.cs : [assembly: ExcludeFromCodeCoverage]

--- CodeCoverageWorkshop.Logic.NUnit.Test/LoginValidatorTests.cs
+++ CodeCoverageWorkshop.Logic.NUnit.Test/LoginValidatorTests.cs
@@ -2,7 +2,10 @@
 using Moq;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+
+//[assembly: ExcludeFromCodeCoverage]
 
 namespace CodeCoverageWorkshop.Logic.NUnit.Test
 {
