125b030cc28a25d382123303ca828da1ee375f40
Łukasz Pękala <lukasz-pekala@users.noreply.github.com>
2021-10-21 13:23:49 +0200

    Examples for #100%CoverageMyth

--- CodeCoverageWorkshop.Logic.xUnit.Test/DiscountProvider2Tests.cs
+++ CodeCoverageWorkshop.Logic.xUnit.Test/DiscountProvider2Tests.cs
@@ -11,10 +11,7 @@ namespace CodeCoverageWorkshop.Logic.xUnit.Test
         {
             // Arrange
             var discountProvider2 = new DiscountProvider2();
-            IList<Product> products = new List<Product>();
-            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
-            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
-            products.Add(new Product() { Name = "Pencil", Price = 41.0m });
+            var products = CreateProductsWithSum101();
             string promoCode = null;
 
             // Act
@@ -24,15 +21,28 @@ namespace CodeCoverageWorkshop.Logic.xUnit.Test
             Assert.Equal(95.95m, result);
         }
 
+        // #100%CoverageMyth FIX for Mutation: a change to "sum >= 100.0m" is not covered
+        //[Fact]
+        //public void CalculateDiscount_ShouldNOTApplyDiscount_ForSum100()
+        //{
+        //    // Arrange
+        //    var discountProvider2 = new DiscountProvider2();
+        //    var products = CreateProductsWithSum100();
+        //    string promoCode = null;
+
+        //    // Act
+        //    var result = discountProvider2.CalculateDiscount(products, promoCode);
+
+        //    // Assert
+        //    Assert.Equal(100.0m, result);
+        //}
+
         [Fact]
         public void CalculateDiscount_ShouldApplyDiscountCode()
         {
             // Arrange
             var discountProvider2 = new DiscountProvider2();
-            IList<Product> products = new List<Product>();
-            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
-            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
-            products.Add(new Product() { Name = "Pencil", Price = 41.0m });
+            var products = CreateProductsWithSum101();
             string promoCode = "HOLIDAYS10";
 
             // Act
@@ -43,21 +53,44 @@ namespace CodeCoverageWorkshop.Logic.xUnit.Test
         }
 
         [Fact]
-        public void CalculateDiscount_ShouldNotApplyDiscount_WhenDiscountCodeInvalid()
+        public void CalculateDiscount_ShouldNotApplyDiscount_WhenDiscountCodeInvalidAndSumLessThan100()
         {
             // Arrange
             var discountProvider2 = new DiscountProvider2();
-            IList<Product> products = new List<Product>();
-            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
-            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
-            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
+            var products = CreateProductsWithSum60();
             string promoCode = "HOLIDAYS101";
 
             // Act
             var result = discountProvider2.CalculateDiscount(products, promoCode);
 
             // Assert
-            Assert.Equal(90.0m, result);
+            Assert.Equal(60.0m, result);
+        }
+
+        private IList<Product> CreateProductsWithSum100()
+        {
+            IList<Product> products = new List<Product>();
+            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
+            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
+            products.Add(new Product() { Name = "Pencil", Price = 40.0m });
+            return products;
+        }
+
+        private IList<Product> CreateProductsWithSum101()
+        {
+            IList<Product> products = new List<Product>();
+            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
+            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
+            products.Add(new Product() { Name = "Pencil", Price = 41.0m });
+            return products;
+        }
+
+        private IList<Product> CreateProductsWithSum60()
+        {
+            IList<Product> products = new List<Product>();
+            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
+            products.Add(new Product() { Name = "Pencil", Price = 30.0m });
+            return products;
         }
     }
 }
--- CodeCoverageWorkshop.Logic/DiscountProvider2.cs
+++ CodeCoverageWorkshop.Logic/DiscountProvider2.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace CodeCoverageWorkshop.Logic
 {
-    // This class has 100% code coverage but mutation score of 90 (calculated by stryker-dotnet)
+    // #100%CoverageMyth: This class has 100% code coverage but mutation score of 90 (calculated by stryker-dotnet)
     public class DiscountProvider2
     {
         public decimal CalculateDiscount(IList<Product> products, string promoCode)
@@ -14,7 +14,7 @@ namespace CodeCoverageWorkshop.Logic
             if (promoCode == "HOLIDAYS10")
                 return sum - sum * 0.10m;
 
-            if (sum > 100.0m) // Mutation: "sum > 100.0m" is not covered
+            if (sum >= 100.0m) // #100%CoverageMyth Mutation: a change to "sum >= 100.0m" is not covered
                 return sum - sum * 0.05m;
 
             return sum;
