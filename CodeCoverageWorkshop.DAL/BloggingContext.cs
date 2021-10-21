54fd25c37446fbb0ea1e373a0eb30254fa01d705
Łukasz Pękala <lukasz-pekala@users.noreply.github.com>
2021-10-21 13:52:30 +0200

    EF Migrations for Code coverage exclusions

---
+++ CodeCoverageWorkshop.DAL/BloggingContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeCoverageWorkshop.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeCoverageWorkshop.DAL
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public string DbPath { get; private set; }
+
+        public BloggingContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}blogging.db";
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
